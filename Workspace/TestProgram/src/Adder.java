e0aca0956f986a7f7f8481875403142c110e878a
Erick Mikoshi <mikoshi9810@gmail.com>
2012-02-22 15:18:00 -0500

    Added some new objects.

--- Workspace/TestProgram/src/Adder.java
+++ Workspace/TestProgram/src/Adder.java
@@ -1,14 +1,23 @@
 import java.io.*; 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Adder {
 	public static void main(String args[]) throws IOException {
-		Integer x = 5;
+		Integer x = 6;
 		Integer y = 6;
+		int a = 8;
+		int b = 10;
 		String name; 
 		BufferedReader reader; 
-		int[] arr = {15, 15, 15, 15, 15, 16, 17};
-		
-		int z = x + y;
+		int[] arr = {15, 15, 15, 16};
+		ArrayList<Integer> arr2 = new ArrayList<Integer> ();
+		arr2.add(1);
+		arr2.add(5);
+		arr2.add(7);
+		ArrayList<ArrayList<Integer>> al = new ArrayList<ArrayList<Integer>>();
+		al.add(arr2);
+		Integer z = x + y;
 		System.out.println(z);
 		System.in.read();
 		//reader = new BufferedReader(new InputStreamReader(System.in)); 
